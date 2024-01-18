18f794663ad2a3da9458bdb34e1f54ded0a6fe77
XSBrave <106927628+XSBrave@users.noreply.github.com>
2024-01-19 02:08:52 +0300

    Python, Type-Related Basic Stuff.

---
+++ Siber-Vatan Python/sibervatan-type.py
@@ -0,0 +1,59 @@
+'''sayi1 = 42
+sayi2 = -17
+print(type(sayi1))'''
+
+###########################################################
+
+'''sayi3 = 1.5
+sayi4 = -5.5
+
+print(type(sayi3))'''
+
+###########################################################
+
+'''dogru_mu = True
+yanlis_mi = False
+
+print(type(dogru_mu))'''
+
+###########################################################
+
+'''dogru_mu = True
+yanlis_mi = False
+
+print(type(yanlis_mi))'''
+
+#############################################################
+'''sayi1 = 42
+sayi2 = -17
+ondalik1 = 3.14
+ondalik2 = - 0.5
+dogru_mu = True
+yanlis_mi = False
+
+
+sayi_float = float(sayi1)
+print("int to float", sayi_float)
+
+sayi_int = int(ondalik1)
+print("float to int", sayi_int)
+
+sayi_bool = int(dogru_mu)
+print("bool to int", sayi_bool)'''
+
+#TYPE Çıktısı > İNT 
+
+###############################################################
+
+'''username = input("Lütfen Kullanıcı Adını Giriniz.")
+print("Welcome",username)'''
+
+#################################################################
+
+'''sayi1 = int(input ("Birinci Sayıyı Giriniz"))
+sayi2 = int(input ("İkinci Sayıyı Giriniz"))
+
+print(sayi1+sayi2)'''
+
+
+                                                       # 17.01.2024 #
